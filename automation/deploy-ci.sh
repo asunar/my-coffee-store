a48f738e4754070cb19d5f7b21bcf3ab47ff91aa
Alper <asunar.git@gmail.com>
2020-10-09 11:01:06 -0500

    Fix ci deploy script/yaml

--- automation/deploy-ci.sh
+++ automation/deploy-ci.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-CLOUDFORMATION_ARTIFACTS_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name==\`CloudFormationArtifactsBucket\`].Value" --output text)
+CLOUDFORMATION_ARTIFACTS_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name==\`CloudformationArtifactsBucket\`].Value" --output text)
 
 if [ -z "$CLOUDFORMATION_ARTIFACTS_BUCKET" ]; then
         echo "Unable to locate Cloudformation Export 'CloudformationArtifactsBucket' - have you setup the account-wide-resources for this account and region?"
@@ -12,10 +12,10 @@ fi
 STACK_NAME=coffee-store-ci
 
 sam.cmd deploy \
-        --stack-name STACK_NAME \
+        --stack-name $STACK_NAME \
         --template-file ci.yaml \
         --capabilities CAPABILITY_IAM \
-        --parameter-overrides CloudFormationArtifactsBucket=$CLOUDFORMATION_ARTIFACTS_BUCKET \
+        --parameter-overrides CloudformationArtifactsBucket=$CLOUDFORMATION_ARTIFACTS_BUCKET \
         --no-fail-on-empty-changeset
 
 echo
