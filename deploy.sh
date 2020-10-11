f70ad17d0424cc1e1394230bcf219a3c9a685d1d
Alper <asunar.git@gmail.com>
2020-10-11 06:54:17 -0500

    Add check for missing artifacts bucket

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,15 @@ fi
 
 CLOUDFORMATION_ARTIFACTS_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name==\`CloudformationArtifactsBucket\`].Value" --output text) 
 
+if [ -z "$CLOUDFORMATION_ARTIFACTS_BUCKET" ]; then
+  echo "Unable to locate Cloudformation Export 'CloudformationArtifactsBucket' - have you setup the account-wide-resources for this account and region?"
+  exit 1
+fi
+
+echo "Deploying stack $STACK_NAME"
+echo "Deploying using Cloudformation artifacts bucket $CLOUDFORMATION_ARTIFACTS_BUCKET"
+echo
+
 sam deploy \
     --stack-name $STACK_NAME \
     --s3-bucket $CLOUDFORMATION_ARTIFACTS_BUCKET \
