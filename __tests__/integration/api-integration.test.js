f9add7fbf271a0afcc1008d872260575c35a8c76
Alper <asunar.git@gmail.com>
2020-10-11 07:37:53 -0500

    Generate ephemeral stack name, delete stack after test run

--- __tests__/integration/api-integration.test.js
+++ __tests__/integration/api-integration.test.js
@@ -1,14 +1,26 @@
 const 
     AWS = require("aws-sdk"),
-    https = require('https')
+    https = require('https'),
+    util = require('util'),
+    exec = util.promisify(require('child_process').exec)
 
+const cloudFormation = new AWS.CloudFormation()
+
+const isUsingEphemeralStack = !process.env.hasOwnProperty('STACK_NAME')
+let stackName
 let apiEndpoint
 
 beforeAll(async () => {
-    const stackName = process.env.hasOwnProperty('STACK_NAME')
-        ? process.env['STACK_NAME']
-        : `coffee-store-${process.env['USER']}`
-    console.log(`Looking for API Gateway in stack [${stackName}]`)
+    // const stackName = process.env.hasOwnProperty('STACK_NAME')
+    //     ? process.env['STACK_NAME']
+    //     : `coffee-store-${process.env['USER']}`
+    // console.log(`Looking for API Gateway in stack [${stackName}]`)
+
+    stackName = generateEphemeralStackName();   
+    console.log('Starting cloudformation deployment of stack', stackName)
+    const { stdout } = await exec(`./deploy.sh ${stackName}`)
+    console.log('Deployment finished')
+    console.log(stdout)
 
     // Get all the details for our stack
     const cloudFormationStacks = await new AWS.CloudFormation().describeStacks({StackName: stackName}).promise()
@@ -40,3 +52,26 @@ function getWithBody(url) {
             response.on('end', () => resolve({...response, ...{body: body}}))
         }).on('error', reject)
     })}
+
+    function generateEphemeralStackName() {
+        const prefix = process.env.hasOwnProperty('STACK_NAME_PREFIX')
+            ? process.env['STACK_NAME_PREFIX']
+            : `coffee-store-it`
+        const now = new Date(),
+            year = now.getFullYear(),
+            month = twoCharacter(now.getMonth() + 1),
+            day = twoCharacter(now.getDate()),
+            hours = twoCharacter(now.getHours()),
+            minutes = twoCharacter(now.getMinutes()),
+            seconds = twoCharacter(now.getSeconds())
+        return `${prefix}-${year}${month}${day}-${hours}${minutes}${seconds}`
+    }
+    
+    function twoCharacter(number) {
+        return number < 10 ? `0${number}` : `${number}`
+    }
+
+afterAll(async () => {
+    console.log('Calling cloudformation to delete stack', stackName)
+    await cloudFormation.deleteStack( { StackName: stackName }).promise()
+})
